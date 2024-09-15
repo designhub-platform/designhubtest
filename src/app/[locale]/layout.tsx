31bad035591bc2cbca9ae38c7abb30f0e5bb1a57
designhub-platform <atendimento.designhub@gmail.com>
2024-09-15 19:49:15 +0000

    Fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
-import { AppHeader } from "@/components/shared/header";
-import { cn } from "@/lib/utils";
+import { BottomMenu } from "@/components/shared/bottom-menu";
+//import { AppHeader } from "@/components/shared/header";
+//import { cn } from "@/lib/utils";
 
 interface PagesLayoutProps {
   children: React.ReactNode;
@@ -9,15 +10,13 @@ export default async function PagesLayout({
   children,
 }: PagesLayoutProps) {
   return (
-    <div className={cn(
-          "min-h-screen bg-background font-sans text-foreground antialiased",
-          
-        )}>
-      <AppHeader />
+    <div className=
+          "bg-background font-sans text-foreground antialiased">
+      
       <main className="flex min-h-screen w-full flex-col items-center justify-center bg-gradient-to-br  from-indigo-50 via-white to-cyan-100 py-32">
         {children}
       </main>
-  
+  <BottomMenu/>
     </div>
   );
 }
--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -21,7 +21,7 @@ export async function AppHeader() {
     <Header.Root session={data}>
       <Header.MenuBarButton />
       <Header.Content />
-      <Header.DesktopMenuBar />
+      <Header.DesktopMenuBar 
     </Header.Root>
   );
 }
